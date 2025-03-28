78040ed06b9d66d2dd468dc7a2c2f4fe7487986d
luzskka <lukasmatos420@gmail.com>
2025-03-28 20:36:03 -0300

    operacoesConjuntos

---
+++ operacoesConjuntos.py
@@ -0,0 +1,13 @@
+# Dados A = {0, 1, 2, 3} e B = {2, 3, 4, 5}, determine A ∪ B.
+A = {0, 1, 2, 3}
+B = {2, 3, 4, 5}
+C = A|B # A u B
+print("A u B = ", C) 
+
+# Dados A = {- 1, 0, 2, 4, 5} e B = {- 2, - 1, 1, 3, 4, 6}, determine A - B e B - A.
+A = {-1, 0, 2, 4, 5}
+B = {-2, -1, 1, 3, 4, 6}
+C = A - B # A menos B
+D = B - A # B menos A
+print("A - B = ", C)
+print("B - A = ", D)
