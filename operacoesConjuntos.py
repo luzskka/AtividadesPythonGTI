430981ef91f3b60fbcdf2d7c9fa7129e0f44da77
luzskka <lukasmatos420@gmail.com>
2025-03-28 22:34:49 -0300

    operacoesConjunto01

--- operacoesConjuntos.py
+++ operacoesConjuntos.py
@@ -1,13 +1,23 @@
-# Dados A = {0, 1, 2, 3} e B = {2, 3, 4, 5}, determine A ∪ B.
+#a) Dados A = {0, 1, 2, 3} e B = {2, 3, 4, 5}, determine A ∪ B.
 A = {0, 1, 2, 3}
 B = {2, 3, 4, 5}
 C = A|B # A u B
 print("A u B = ", C) 
 
-# Dados A = {- 1, 0, 2, 4, 5} e B = {- 2, - 1, 1, 3, 4, 6}, determine A - B e B - A.
+#b) Dados A = {- 1, 0, 2, 4, 5} e B = {- 2, - 1, 1, 3, 4, 6}, determine A - B e B - A.
 A = {-1, 0, 2, 4, 5}
 B = {-2, -1, 1, 3, 4, 6}
 C = A - B # A menos B
 D = B - A # B menos A
 print("A - B = ", C)
 print("B - A = ", D)
+
+#c) Considere os conjuntos A = {0, 1, 2, 3, 4, 5, 6}, B = {1, 2}, C = {2, 3, 4} e D = {4, 5}, determine
+# (A − B) ∩ (C ∩ D).
+A = {1, 2, 3, 4, 5, 6}
+B = {1, 2}
+C = {2, 3, 4}
+D = {4, 5}
+E = A - B
+F = C & D 
+G = E & F
